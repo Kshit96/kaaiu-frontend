67ac247302f51105188a2e7ec57783ec5895d7a2
kshitiz <kshit96@gmail.com>
2020-06-21 03:48:18 +0700

    Add introduction text and navigation text to navigation section

--- src/components/NavigationSection.js
+++ src/components/NavigationSection.js
@@ -2,19 +2,43 @@ import React from 'react';
 import './NavigationSection.css'
 import {Col, Row} from "antd";
 import logo from "../assets/logo type 2 .png"
+import {Typography, Space} from 'antd';
+import MenuOutlined from "@ant-design/icons/es/icons/MenuOutlined";
+import {blue} from "color-name";
+
+const {Title, Text, Link} = Typography;
 
 export default function NavigationSection(props) {
   return (
-      <Row className={'navigation-background'}>
-        <Row span={24}>
-          <Col span={8}>
-            <img src={logo} className={"logo"} width={"20"} height={"20"}/>
-          </Col >
-          <Col span={8}>
-          </Col>
-          <Col span={8}>
-          </Col>
-        </Row>
+      <Row span={24} className={'navigation-background'}>
+        <Col span={2}>
+          <img src={logo} className={"logo"}/>
+        </Col>
+        <Col push={4} span={12}>
+          <Row style={{height: 50}} justify={"space-around"} align={"bottom"}>
+            <Text style={{fontSize: 20}}>about</Text>
+            <Text style={{fontSize: 20}}>how it works</Text>
+            <Text style={{fontSize: 20}}>explore</Text>
+          </Row>
+          <Row justify={"center"} align={"bottom"} className={"company-name"}>
+            <Space direction="vertical" style={{textAlign: "center"}}>
+              <Title level={1}>Kaaiu</Title>
+              <Text >At Kaaiu, We connect you
+                directly to furniture manufacturers
+                around the region<br/>
+              making that amazing furniture idea of
+                yours a reality</Text>
+            </Space>
+          </Row>
+        </Col>
+        <Col push={4} span={6}>
+          <Row style={{height: 70}} justify={"space-around"} align={"bottom"}>
+            <Text style={{fontSize: 20, color: "#1E90FF"}}>Get Newsletter</Text>
+            <Text style={{fontSize: 20, color: "#1E90FF"}}>Shop</Text>
+            <Text style={{fontSize: 20, color: "#1E90FF"}}>Sign In</Text>
+            <MenuOutlined style={{fontSize: 30}}/>
+          </Row>
+        </Col>
       </Row>
   )
 }
